968c807edc1985d0fd63d814811aaaeff3c8dc20
Руслан Мусаев <ru.mu2005@mail.ru>
2023-11-09 17:44:46 +0300

    Task6

---
+++ Tyuiu.MusaevRS.Sprint4.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MusaevRS.Sprint4.Task6.V11.Lib
+{
+    public class DataService : ISprint4Task6V11
+    {
+        public int Calculate(string[] array)
+        {
+            int count = array.Count(item => item.Length == 5);
+            return count;
+                   
+
+        }
+    }
+}
---
+++ Tyuiu.MusaevRS.Sprint4.Task6.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MusaevRS.Sprint4.Task6.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MusaevRS.Sprint4.Task6.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7564e5d-4e00-4d73-b605-87182399aa97")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
